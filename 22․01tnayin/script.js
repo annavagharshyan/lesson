88fbed5cd4aeec80f047aa2b22f162237465a833
annavagharshyan <vagharshyananna@gmail.com>
2025-01-21 18:49:44 +0400

    Add files via upload

---
+++ 22․01tnayin/script.js
@@ -0,0 +1,18 @@
+// var b = prompt();
+// for(var a = 0; a <= b; a++ ){
+//    console.log(a);
+// }
+
+// var c = prompt();
+// for(var a = c; a >= 1; a--){
+//    console.log(a);
+// }
+
+for(var a = 1; a <=10; a++){
+    if(a % 2 == 1){
+       document.write("<h1>Hello World!</h1>")
+    }else{
+       document.write("<h3>Hello World!</h3>")
+    }
+ }
+ 
