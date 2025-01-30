733881d5bee374eace95946b917c3f176998e5ff
annavagharshyan <vagharshyananna@gmail.com>
2025-01-30 22:40:21 +0400

    Add files via upload

--- 31.01tnayin/script.js
+++ 31.01tnayin/script.js
@@ -88,95 +88,14 @@ var obj10 = {
   language: "French"
 };
 
-
+var arr = [obj1, obj2, obj3, obj4, obj5, obj6, obj7, obj8, obj9, obj10];
+for (var a = 0; a < arr.length; a++) {
   document.write("<ul>");
-  document.write("<li> Name: ",obj1["name"],"</li>");
-  document.write("<li> Surname: ",obj1["surname"],"</li>");
-  document.write("<li> Age: ",obj1["age"],"</li>");
-  document.write("<li> City: ",obj1["city"],"</li>");
-  document.write("<li> Job: ",obj1["job"],"</li>");
-  document.write("<li> Language: ",obj1["language"],"</li>");
+  document.write("<li> name: ", arr[a]["name"], "</li>");
+  document.write("<li> surname: ", arr[a]["surname"], "</li>");
+  document.write("<li> age: ", arr[a]["age"], "</li>");
+  document.write("<li> city: ", arr[a]["city"], "</li>");
+  document.write("<li> job: ", arr[a]["job"], "</li>");
+  document.write("<li> language: ", arr[a]["language"], "</li>");
   document.write("</ul>");
-
-  document.write("<ul>");
-  document.write("<li> Name: ",obj2["name"],"</li>");
-  document.write("<li> Surname: ",obj2["surname"],"</li>");
-  document.write("<li> Age: ",obj2["age"],"</li>");
-  document.write("<li> City: ",obj2["city"],"</li>");
-  document.write("<li> Job: ",obj2["job"],"</li>");
-  document.write("<li> Language: ",obj2["language"],"</li>");
-  document.write("</ul>");
-
-  document.write("<ul>");
-  document.write("<li> Name: ",obj3["name"],"</li>");
-  document.write("<li> Surname: ",obj3["surname"],"</li>");
-  document.write("<li> Age: ",obj3["age"],"</li>");
-  document.write("<li> City: ",obj3["city"],"</li>");
-  document.write("<li> Job: ",obj3["job"],"</li>");
-  document.write("<li> Language: ",obj3["language"],"</li>");
-  document.write("</ul>");
-
-  document.write("<ul>");
-  document.write("<li> Name: ",obj4["name"],"</li>");
-  document.write("<li> Surname: ",obj4["surname"],"</li>");
-  document.write("<li> Age: ",obj4["age"],"</li>");
-  document.write("<li> City: ",obj4["city"],"</li>");
-  document.write("<li> Job: ",obj4["job"],"</li>");
-  document.write("<li> Language: ",obj4["language"],"</li>");
-  document.write("</ul>");
-
-  document.write("<ul>");
-  document.write("<li> Name: ",obj5["name"],"</li>");
-  document.write("<li> Surname: ",obj5["surname"],"</li>");
-  document.write("<li> Age: ",obj5["age"],"</li>");
-  document.write("<li> City: ",obj5["city"],"</li>");
-  document.write("<li> Job: ",obj5["job"],"</li>");
-  document.write("<li> Language: ",obj5["language"],"</li>");
-  document.write("</ul>");
-
-  document.write("<ul>");
-  document.write("<li> Name: ",obj6["name"],"</li>");
-  document.write("<li> Surname: ",obj6["surname"],"</li>");
-  document.write("<li> Age: ",obj6["age"],"</li>");
-  document.write("<li> City: ",obj6["city"],"</li>");
-  document.write("<li> Job: ",obj6["job"],"</li>");
-  document.write("<li> Language: ",obj6["language"],"</li>");
-  document.write("</ul>");
-
-  document.write("<ul>");
-  document.write("<li> Name: ",obj7["name"],"</li>");
-  document.write("<li> Surname: ",obj7["surname"],"</li>");
-  document.write("<li> Age: ",obj7["age"],"</li>");
-  document.write("<li> City: ",obj7["city"],"</li>");
-  document.write("<li> Job: ",obj7["job"],"</li>");
-  document.write("<li> Language: ",obj7["language"],"</li>");
-  document.write("</ul>");
-
-  document.write("<ul>");
-  document.write("<li> Name: ",obj8["name"],"</li>");
-  document.write("<li> Surname: ",obj8["surname"],"</li>");
-  document.write("<li> Age: ",obj8["age"],"</li>");
-  document.write("<li> City: ",obj8["city"],"</li>");
-  document.write("<li> Job: ",obj8["job"],"</li>");
-  document.write("<li> Language: ",obj8["language"],"</li>");
-  document.write("</ul>");
-
-  document.write("<ul>");
-  document.write("<li> Name: ",obj9["name"],"</li>");
-  document.write("<li> Surname: ",obj9["surname"],"</li>");
-  document.write("<li> Age: ",obj9["age"],"</li>");
-  document.write("<li> City: ",obj9["city"],"</li>");
-  document.write("<li> Job: ",obj9["job"],"</li>");
-  document.write("<li> Language: ",obj9["language"],"</li>");
-  document.write("</ul>");
-
-  document.write("<ul>");
-  document.write("<li> Name: ",obj10["name"],"</li>");
-  document.write("<li> Surname: ",obj10["surname"],"</li>");
-  document.write("<li> Age: ",obj10["age"],"</li>");
-  document.write("<li> City: ",obj10["city"],"</li>");
-  document.write("<li> Job: ",obj10["job"],"</li>");
-  document.write("<li> Language: ",obj10["language"],"</li>");
-  document.write("</ul>");
-
-
+}
