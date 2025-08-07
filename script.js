eb2429ed8e6171ad55e9df57ed4010169245b91a
annavagharshyan <vagharshyananna@gmail.com>
2025-08-07 23:40:20 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,558 @@
+const menuData = {
+  meat: [
+    {
+      img: "images/image1.png",
+      name: "BBQ Steak",
+      desc: "Հյութալի տավարի ստեյք՝ ածխով խորոված, թարմ խոտաբույսերով և սմբուկով",
+      price: 9500,
+    },
+    {
+      img: "images/image2.png",
+      name: "Lamb Chops",
+      desc: "Գառան կողիկներ՝ սխտորով և սոխով մարինացված, ածխով խորոված",
+      price: 3000,
+    },
+    {
+      img: "images/image4.png",
+      name: "Spicy Chicken Kebab",
+      desc: "Կծու համեմունքներով շամփուրի վրա խորոված հավի մսիկ",
+      price: 4000,
+    },
+    {
+      img: "images/image5.png",
+      name: "Grilled Beef Ribs",
+      desc: "Մեղրով և քաղցր BBQ սոուսով պատրաստված տավարի կողիկներ",
+      price: 2500,
+    },
+    {
+      img: "images/image6.png",
+      name: "Herb Marinated Pork Skewers",
+      desc: "Թարմ կանաչիներով մարինացված և ածխով խորոված խոզի մսիկ",
+      price: 6500,
+    },
+    {
+      img: "images/image7.png",
+      name: "Lamb Kofta",
+      desc: "Գառան աղացած միս՝ համեմունքներով և խտացրած համով ապուր",
+      price: 2000,
+    },
+    {
+      img: "images/image8.png",
+      name: "Beef Kebab",
+      desc: "Տավարի մսով շամփուր՝ խոզի ճարպով և թթու սոխով",
+      price: 3000,
+    },
+    {
+      img: "images/image9.png",
+      name: "Chicken Roll",
+      desc: "Հավի ֆիլե՝ պանրով և կանաչիներով լցոնված, թխված և խորոված",
+      price: 2500,
+    },
+    {
+      img: "images/image10.png",
+      name: "Honey Glazed Pork Ribs",
+      desc: "Մեղրով և քաղցր-կծու սոուսով խոզի կողիկներ",
+      price: 7000,
+    },
+    {
+      img: "images/image11.png",
+      name: "Grilled Vegetables",
+      desc: "Թարմ սմբուկ, պղպեղ, ցուկկինի և սոխ՝ ածխով խորոված և խոտաբույսերով համեմված",
+      price: 5000,
+    },
+    {
+      img: "images/image12.png",
+      name: "Farmhouse Omelette",
+      desc: "Թարմ ձվով, պանրով, լոլիկով և կանաչիներով պատրաստված ձվածեղ",
+      price: 3500,
+    },
+    {
+      img: "images/image13.png",
+      name: "Pork Shashlik",
+      desc: "Խոզի մսով շաշլիկ՝ սոխով, լոլիկով և խոտաբույսերով",
+      price: 2500,
+    },
+    {
+      img: "images/image14.png",
+      name: "Barbecue Lamb Ribs",
+      desc: "Գառան կողիկներ՝ թանձր լոլիկի սոուսով",
+      price: 4400,
+    },
+    {
+      img: "images/image15.png",
+      name: "Beef Medallions",
+      desc: "Տավարի ֆիլեի խտացրած կտորներ՝ թարմ խոտաբույսերով",
+      price: 6400,
+    },
+    {
+      img: "images/image16.png",
+      name: "Chicken Shashlik",
+      desc: "Հյութալի հավի շաշլիկ՝ քիմիոնով, աղացած պղպեղով և սոխով",
+      price: 2300,
+    },
+    {
+      img: "images/image17.png",
+      name: "Creamy Mushroom Soup",
+      desc: "Նուրբ սնկով կրեմ-ապուր՝ թարմ սերուցքով, բուրավետ խոտաբույսերով",
+      price: 5200,
+    },
+  ],
+  salads: [
+    {
+      img: "images/image19.png",
+      name: "Greek Salad",
+      desc: "Feta, olives, tomatoes",
+      price: 5000,
+    },
+    {
+      img: "images/image20.png",
+      name: "Caesar Salad",
+      desc: "Parmesan and croutons",
+      price: 2800,
+    },
+    {
+      img: "images/image21.png",
+      name: "Greek Salad",
+      desc: "Feta, olives, tomatoes",
+      price: 3000,
+    },
+    {
+      img: "images/image22.png",
+      name: "Greek Salad",
+      desc: "Feta, olives, tomatoes",
+      price: 2700,
+    },
+    {
+      img: "images/image23.png",
+      name: "Greek Salad",
+      desc: "Feta, olives, tomatoes",
+      price: 1500,
+    },
+    {
+      img: "images/image24.png",
+      name: "Greek Salad",
+      desc: "Feta, olives, tomatoes",
+      price: 3500,
+    },
+    {
+      img: "images/image25.png",
+      name: "Greek Salad",
+      desc: "Feta, olives, tomatoes",
+      price: 2600,
+    },
+    {
+      img: "images/image26.png",
+      name: "Greek Salad",
+      desc: "Feta, olives, tomatoes",
+      price: 2200,
+    },
+    {
+      img: "images/image27.png",
+      name: "Greek Salad",
+      desc: "Feta, olives, tomatoes",
+      price: 2000,
+    },
+    {
+      img: "images/image28.png",
+      name: "Greek Salad",
+      desc: "Feta, olives, tomatoes",
+      price: 2900,
+    },
+    {
+      img: "images/image29.png",
+      name: "Greek Salad",
+      desc: "Feta, olives, tomatoes",
+      price: 2400,
+    },
+    {
+      img: "images/image30.png",
+      name: "Greek Salad",
+      desc: "Feta, olives, tomatoes",
+      price: 3000,
+    },
+    {
+      img: "images/image31.png",
+      name: "Greek Salad",
+      desc: "Feta, olives, tomatoes",
+      price: 2000,
+    },
+    {
+      img: "images/image32.png",
+      name: "Greek Salad",
+      desc: "Feta, olives, tomatoes",
+      price: 3600,
+    },
+  ],
+  fish: [
+    {
+      img: "images/image39.png",
+      name: "Salmon Classic Set",
+      desc: "Վագրային սալմոն նիգիրի և մակի, կիտրոնի շերտով",
+      price: 11000,
+    },
+    {
+      img: "images/image34.png",
+      name: "Salmon Nigiri Combo",
+      desc: "Սոյայով մատուցվող նիգիրի սալմոն՝ ավոկադոյով",
+      price: 15000,
+    },
+    {
+      img: "images/image35.png",
+      name: "Salmon Roll Party",
+      desc: "Խեցգետնով ու սալմոնով մակի՝ սև քունջութով",
+      price: 12500,
+    },
+    {
+      img: "images/image36.png",
+      name: "Red Fire Roll",
+      desc: "Բոցավառված սալմոնով ռոլլ, բալիկ վրան",
+      price: 10500,
+    },
+    {
+      img: "images/image37.png",
+      name: "Samurai Deluxe",
+      desc: "Սուշի և ռոլլի համադրություն՝ հատիկավոր սոյայով",
+      price: 14000,
+    },
+    {
+      img: "images/image38.png",
+      name: "Volcano Salmon",
+      desc: "Հարավկորեական ոճով սալմոն՝ բիբարով",
+      price: 13000,
+    },
+    {
+      img: "images/image40.png",
+      name: "Cucumber Light Roll",
+      desc: "Թարմ վարունգով բուսական ռոլլ՝ հոսոմակի տեսքով",
+      price: 5000,
+    },
+    {
+      img: "images/image41.png",
+      name: "Grilled Salmon",
+      desc: "Fresh salmon with lemon",
+      price: 6000,
+    },
+    {
+      img: "images/image42.png",
+      name: "Grilled Salmon",
+      desc: "Fresh salmon with lemon",
+      price: 3300,
+    },
+    {
+      img: "images/image43.png",
+      name: "Grilled Salmon",
+      desc: "Fresh salmon with lemon",
+      price: 5500,
+    },
+    {
+      img: "images/image44.png",
+      name: "Grilled Salmon",
+      desc: "Fresh salmon with lemon",
+      price: 4500,
+    },
+    {
+      img: "images/image45.png",
+      name: "Grilled Salmon",
+      desc: "Fresh salmon with lemon",
+      price: 3500,
+    },
+    {
+      img: "images/image46.png",
+      name: "Grilled Salmon",
+      desc: "Fresh salmon with lemon",
+      price: 4200,
+    },
+    {
+      img: "images/image47.png",
+      name: "Grilled Salmon",
+      desc: "Fresh salmon with lemon",
+      price: 6200,
+    },
+    {
+      img: "images/image48.png",
+      name: "Grilled Salmon",
+      desc: "Fresh salmon with lemon",
+      price: 6200,
+    },
+    {
+      img: "images/image49.png",
+      name: "Grilled Salmon",
+      desc: "Fresh salmon with lemon",
+      price: 6200,
+    },
+    {
+      img: "images/image50.png",
+      name: "Grilled Salmon",
+      desc: "Fresh salmon with lemon",
+      price: 6200,
+    },
+    {
+      img: "images/image51.png",
+      name: "Grilled Salmon",
+      desc: "Fresh salmon with lemon",
+      price: 6200,
+    },
+  ],
+  pizza: [
+    {
+      img: "images/image52.png",
+      name: "Margherita Pizza",
+      desc: "Classic Italian pizza with tomatoes and mozzarella",
+      price: 3100,
+    },
+    {
+      img: "images/image53.png",
+      name: "Margherita Pizza",
+      desc: "Classic Italian pizza with tomatoes and mozzarella",
+      price: 2700,
+    },
+    {
+      img: "images/image54.png",
+      name: "Margherita Pizza",
+      desc: "Classic Italian pizza with tomatoes and mozzarella",
+      price: 2100,
+    },
+    {
+      img: "images/image55.png",
+      name: "Margherita Pizza",
+      desc: "Classic Italian pizza with tomatoes and mozzarella",
+      price: 2900,
+    },
+    {
+      img: "images/image56.png",
+      name: "Margherita Pizza",
+      desc: "Classic Italian pizza with tomatoes and mozzarella",
+      price: 3700,
+    },
+    {
+      img: "images/image57.png",
+      name: "Margherita Pizza",
+      desc: "Classic Italian pizza with tomatoes and mozzarella",
+      price: 2600,
+    },
+    {
+      img: "images/image58.png",
+      name: "Margherita Pizza",
+      desc: "Classic Italian pizza with tomatoes and mozzarella",
+      price: 2600,
+    },
+    {
+      img: "images/image59.png",
+      name: "Margherita Pizza",
+      desc: "Classic Italian pizza with tomatoes and mozzarella",
+      price: 2100,
+    },
+    {
+      img: "images/image60.png",
+      name: "Margherita Pizza",
+      desc: "Classic Italian pizza with tomatoes and mozzarella",
+      price: 3200,
+    },
+    {
+      img: "images/image61.png",
+      name: "Margherita Pizza",
+      desc: "Classic Italian pizza with tomatoes and mozzarella",
+      price: 2500,
+    },
+  ],
+  beverages: [
+    {
+      img: "images/image62.png",
+      name: "Watermelon iced tea",
+      desc: "Freshly squeezed orange juice",
+      price: 1200,
+    },
+    {
+      img: "images/image63.png",
+      name: "Orange Juice",
+      desc: "Chilled natural apple juice",
+      price: 1200,
+    },
+    {
+      img: "images/image64.png",
+      name: "Lemonade",
+      desc: "Homemade sparkling lemonade",
+      price: 1300,
+    },
+    {
+      img: "images/image65.png",
+      name: "Iced Tea",
+      desc: "Refreshing cold brewed tea with lemon",
+      price: 1100,
+    },
+    {
+      img: "images/image66.png",
+      name: "Mineral tea",
+      desc: "Still or sparkling mineral water",
+      price: 800,
+    },
+    {
+      img: "images/image67.png",
+      name: "Cola",
+      desc: "Classic cola beverage",
+      price: 1000,
+    },
+    {
+      img: "images/image68.png",
+      name: "Espresso",
+      desc: "Strong and aromatic Italian espresso",
+      price: 900,
+    },
+    {
+      img: "images/image69.png",
+      name: "Cappuccino",
+      desc: "Creamy cappuccino with milk foam",
+      price: 1200,
+    },
+    {
+      img: "images/image70.png",
+      name: "Hot Chocolate",
+      desc: "Rich and smooth hot chocolate",
+      price: 1300,
+    },
+    {
+      img: "images/image71.png",
+      name: "Pineapple Juice",
+      desc: "Tropical fresh pineapple juice",
+      price: 1400,
+    },
+    {
+      img: "images/image72.png",
+      name: "Mojito Mocktail",
+      desc: "Non-alcoholic mojito with mint and lime",
+      price: 1600,
+    },
+  ],
+  wine: [
+    {
+      img: "images/image73.png",
+      name: "Areni Dry Red",
+      desc: "Classic Armenian dry red wine from Areni grape",
+      price: 7500,
+    },
+    {
+      img: "images/image74.png",
+      name: "Voskehat White",
+      desc: "Elegant white wine made from Voskehat variety",
+      price: 8200,
+    },
+    {
+      img: "images/image75.png",
+      name: "Kangun Rosé",
+      desc: "Light and fresh rosé wine with floral aromas",
+      price: 6900,
+    },
+    {
+      img: "images/image76.png",
+      name: "Reserve Red Blend",
+      desc: "Aged blend of Armenian Areni and Sireni grapes",
+      price: 9500,
+    },
+    {
+      img: "images/image77.png",
+      name: "White Semi-Sweet",
+      desc: "Delicate semi-sweet wine with pear and melon notes",
+      price: 7200,
+    },
+    {
+      img: "images/image78.png",
+      name: "Pomegranate Wine",
+      desc: "Unique Armenian wine made from pomegranate juice",
+      price: 8700,
+    },
+    {
+      img: "images/image79.png",
+      name: "Muscat Dessert Wine",
+      desc: "Sweet Muscat wine with rich honey and apricot flavors",
+      price: 9800,
+    },
+  ],
+};
+
+function showCategory(category) {
+  const menuGrid = document.querySelector(".menu-grid");
+  menuGrid.innerHTML = "";
+  menuData[category].forEach((item) => {
+    menuGrid.innerHTML += `
+      <div class="menu-item">
+  <!-- Սրտիկը -->
+  <div class="favorite-icon" onclick="toggleFavorite(this, '${item.name}')">
+    <i class="fa-regular fa-heart"></i>
+  </div>
+  <img src="${item.img}" alt="">
+  <h3>${item.name}</h3>
+  <p>${item.desc}</p>
+
+  <div class="menu-actions">
+    <div class="quantity-control">
+      <button class="qty-btn" onclick="decreaseQty(this)">−</button>
+      <span class="qty-value">1</span>
+      <button class="qty-btn" onclick="increaseQty(this)">+</button>
+    </div>
+    <div class="price-value">${item.price.toLocaleString()} ֏</div>
+  </div>
+
+  <button class="add-to-cart">
+    <i class="fa-solid fa-bag-shopping"></i> Ավելացնել
+  </button>
+</div>
+
+    `;
+  });
+  
+
+  
+
+  document
+    .querySelectorAll("nav.number a")
+    .forEach((link) => link.classList.remove("active"));
+  const activeLink = document.querySelector(
+    `nav.number a[data-category="${category}"]`
+  );
+  if (activeLink) activeLink.classList.add("active");
+}
+
+window.onload = () => {
+  showCategory("meat");
+};
+
+
+function increaseQty(button) {
+  const qtySpan = button.parentElement.querySelector(".qty-value");
+  let qty = parseInt(qtySpan.innerText);
+  qtySpan.innerText = qty + 1;
+}
+
+function decreaseQty(button) {
+  const qtySpan = button.parentElement.querySelector(".qty-value");
+  let qty = parseInt(qtySpan.innerText);
+  if (qty > 1) {
+    qtySpan.innerText = qty - 1;
+  }
+}
+
+
+let favoriteItems = [];
+
+function toggleFavorite(el, itemName) {
+  const icon = el.querySelector("i");
+  const badge = document.querySelector(".top-icons .badge");
+
+  icon.classList.toggle("fa-solid");
+  icon.classList.toggle("fa-regular");
+
+  if (favoriteItems.includes(itemName)) {
+    favoriteItems = favoriteItems.filter(item => item !== itemName);
+  } else {
+    favoriteItems.push(itemName);
+  }
+
+  badge.textContent = favoriteItems.length;
+}
+
+
+
+function toggleFilter(element) {
+  const box = element.parentElement;
+  box.classList.toggle('closed');
+}
